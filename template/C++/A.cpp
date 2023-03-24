d164bf885b7cde4eeaf7ddb2a6686c7e93271af2
Rakibul Hasan <101rakibulhasan@gmail.com>
2023-03-25 01:58:53 +0600

    Initial Template Commit

---
+++ template/C++/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+signed main()
+{
+  ios::sync_with_stdio(false);
+  cout.tie(nullptr);
+  
+  //start from here...
+
+  return 0;
+}
---
+++ template/C++/G.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+signed main()
+{
+  ios::sync_with_stdio(false);
+  cout.tie(nullptr);
+  int tt=1;
+  cin>>tt;
+  while(tt--)
+  {
+     //start from here...
+  }
+  return 0;
+}
